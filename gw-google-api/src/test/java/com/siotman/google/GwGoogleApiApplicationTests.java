f9d56db4db93790e7089ab76849a29fea5a71f9f
SSK <siotman0728@gmail.com>
2022-02-06 10:53:08 +0900

    init.

---
+++ gw-google-api/src/test/java/com/siotman/google/GwGoogleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siotman.google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwGoogleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
