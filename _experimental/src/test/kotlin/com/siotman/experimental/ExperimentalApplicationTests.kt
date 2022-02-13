b3c47568eaddb306e05d0d59eafa2b1162afba50
Seungshin Kim <ian@lendit.co.kr>
2022-02-13 09:43:09 +0900

    gradle 기본 코틀린 플러그인 추가 - TODO : 공통 디펜덴시 정리 (#2)

---
+++ _experimental/src/test/kotlin/com/siotman/experimental/ExperimentalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.siotman.experimental
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExperimentalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
