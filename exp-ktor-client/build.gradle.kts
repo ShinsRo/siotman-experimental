538182c5f5cc7f1fc135b51185dfd19589194c37
ssk <ssk@ian-mac.local>
2023-01-13 08:23:53 +0900

    ktor-client 실험실 오픈

---
+++ exp-ktor-client/build.gradle.kts
@@ -0,0 +1,30 @@
+val ktorVersion: String by rootProject
+val kotlinVersion: String by rootProject
+
+plugins {
+    application
+
+    kotlin("jvm")
+}
+
+group = "com.siotman"
+version = "0.1.0"
+
+application {
+    mainClass.set("com.siotman.ktor.client.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
