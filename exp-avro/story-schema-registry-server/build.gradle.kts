7a5ec16c876e1f73268bf62fa649befcf47a2b3b
siotman <siotman0728@gmail.com>
2023-01-20 11:59:21 +0900

    dependency set up

--- exp-avro/story-schema-registry-server/build.gradle.kts
+++ exp-avro/story-schema-registry-server/build.gradle.kts
@@ -1,5 +1,10 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("java")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+
+    id("org.springframework.boot")
 }
 
 group = "com.siotman.experimental.exp-avro"
@@ -10,9 +15,29 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    val spring_boot_version: String by rootProject
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:$spring_boot_version"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    val spring_cloud_version: String by rootProject
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:$spring_cloud_version"))
+    implementation("org.springframework.cloud:spring-cloud-stream-schema-registry-server")
+
     val junit_version: String by rootProject
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit_version")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit_version")
+    implementation(platform("org.junit:junit-bom:$junit_version"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 }
 
 tasks.getByName<Test>("test") {
