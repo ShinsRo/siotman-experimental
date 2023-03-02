7046ef28550f6560d947c3e32b57a400d8e0ac25
siotman <siotman0728@gmail.com>
2023-03-02 18:19:41 +0900

    refactor(DirtyReadTests): 테스트 공통유틸 따로 정리함에 따른 directive 개별정의 제거
    1. Employees 스키마 개별 클래스로 정의
    2. 디비 테스트 세션 정보, 격리수준 등 상수 값 ReadPhenomenaTestUtils 로 이동
    3. Directive -> TxScenario 이름변경
    4. 테스트 시나리오 제어 클래스, 유틸 정의를 ReadPhenomenaTestUtils.kt 파일 스코프로 이동

---
+++ exp-mysql-read-phenomena/src/main/kotlin/com/siotman/experimental/mysql/phenomena/Employees.kt
@@ -0,0 +1,34 @@
+package com.siotman.experimental.mysql.phenomena
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.plus
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.update
+
+class Employee(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Employee>(Employees)
+
+    val fullName by Employees.fullName
+    var daysOff by Employees.daysOff
+}
+
+object Employees : IntIdTable() {
+    val fullName = varchar("full_name", 30)
+    val daysOff = integer("days_off")
+
+    // 유저의 사용 휴가 일수 조회
+    fun findDaysOff(fullName: String): Int = select {
+        Employees.fullName eq fullName
+    }.single()[daysOff]
+
+    // 사용 휴가 일수 + 1
+    fun increaseDaysOff(fullName: String, amount: Int) = update(where = {
+        Employees.fullName eq fullName
+    }) {
+        it.update(daysOff, daysOff + amount)
+    }
+}
+
--- exp-mysql-read-phenomena/src/test/kotlin/com/siotman/experimental/mysql/phenomena/DirtyReadTests.kt
+++ exp-mysql-read-phenomena/src/test/kotlin/com/siotman/experimental/mysql/phenomena/DirtyReadTests.kt
@@ -1,111 +1,28 @@
 package com.siotman.experimental.mysql.phenomena
 
+import com.siotman.experimental.mysql.phenomena.ReadPhenomenaTestUtils.connectionValueOf
+import com.siotman.experimental.mysql.phenomena.ReadPhenomenaTestUtils.dbUrl
+import com.siotman.experimental.mysql.phenomena.ReadPhenomenaTestUtils.shinsRoFullname
+import com.siotman.experimental.mysql.phenomena.ReadPhenomenaTestUtils.userForKarina
+import com.siotman.experimental.mysql.phenomena.ReadPhenomenaTestUtils.userForShins
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.headers
 import io.kotest.data.row
 import io.kotest.data.table
+import io.kotest.matchers.ints.shouldBeExactly
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.plus
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.TransactionManager
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.sql.Connection
 
-private object DirtyReadEmployeeTable : IntIdTable() {
-    val fullName = varchar("full_name", 30)
-    val offDays = integer("off_days")
-}
-
-private typealias Employees = DirtyReadEmployeeTable
-
-private fun connectionValueOf(intValue: Int) = when (intValue) {
-    Connection.TRANSACTION_READ_UNCOMMITTED -> "READ_UNCOMMITTED"
-    Connection.TRANSACTION_READ_COMMITTED -> "READ_COMMITTED"
-    Connection.TRANSACTION_REPEATABLE_READ -> "REPEATABLE_READ"
-    Connection.TRANSACTION_SERIALIZABLE -> "SERIALIZABLE"
-    else -> throw Exception("다루지 않거나 정의하지 않은 격리수준입니다.")
-}
-
 class DirtyReadTests : StringSpec({
-    // Database 설정
-    // 두 세션, shinsro 의 세션과 karina 의 세션이 존재함을 가정한다.
-    val dbUrl = "jdbc:mysql://localhost:3306/read_phenomena_test"
-    val userForShins = "shinsro"
-    val userForKarina = "karina"
 
-    val dbSessions = mapOf(
-        userForShins to Database.connect(url = dbUrl, user = userForShins),
-        userForKarina to Database.connect(url = dbUrl, user = userForKarina)
-    )
-
-    // 직원 신스로 정보 초기화 함수
-    val shinsRoFullname = "Shins Ro"
-    fun Transaction.initShinsRo() {
-        Employees.replace { employees ->
-            employees[id] = 1
-            employees[fullName] = shinsRoFullname
-            employees[offDays] = 0
-        }
-    }
-
-    // 테스트 시나리오 제어를 위한 코루틴 인바운드 이벤트 채널 정의
-    class TxDirective(val statement: suspend Transaction.(username: String) -> Unit) {
-        suspend fun execute(context: Transaction, username: String) = statement.invoke(context, username)
-    }
-
-    val coroutineInBoundChannels = mapOf<String, Channel<TxDirective>>(
-        // DB 유저 이름 to 실행할 함수
-        userForShins to Channel(),
-        userForKarina to Channel()
-    )
-
-    val directives = object {
-        // 이벤트 행위 1: 신스로의 휴가일수 조회
-        val selectShinsRoOffDays = TxDirective { username ->
-            val statement = Employees.select(where = { Employees.fullName eq shinsRoFullname })
-            val offDays = statement.single()[Employees.offDays]
-
-            println("[$username] $shinsRoFullname 의 휴가일수는 $offDays 입니다.")
-        }
-
-        // 이벤트 행위 2: 신스로의 휴가일수 + 1
-        val increaseShinsRoOffDaysOne = TxDirective { username ->
-            Employees.update(where = { Employees.fullName eq shinsRoFullname }) {
-                it.update(Employees.offDays, Employees.offDays + 1)
-            }
-
-            println("[$username] $shinsRoFullname 의 휴가일수를 +1 했습니다.")
-        }
-
-        // 이벤트 행위 3: commit Tx
-        val commit = TxDirective { username ->
-            commit()
-
-            println("[$username] 트랜젝션 Commit.")
-        }
-    }
-
-    // 이벤트 행위 서빙 시작함수
-    suspend fun receiveDirectivesUntilCommit(username: String, isolation: Int) {
-        val session = dbSessions[username]
-        val inbound = coroutineInBoundChannels[username]!!
-        println("[$username] is now taking directives.")
-
-        newSuspendedTransaction(db = session, transactionIsolation = isolation) {
-            do {
-                val directive = inbound.receive()
-                directive.execute(this, username)
-            } while (directive != directives.commit)
-        }
-    }
-
-    // === 테스트 케이스 시작 ===
     "Dirty Reads 는 READ_UNCOMMITTED 격리수준에서만 발생한다" {
         forAll(
             table(
@@ -113,6 +30,7 @@ class DirtyReadTests : StringSpec({
                 row(Connection.TRANSACTION_READ_UNCOMMITTED, Connection.TRANSACTION_READ_COMMITTED),
                 row(Connection.TRANSACTION_READ_COMMITTED, Connection.TRANSACTION_READ_COMMITTED),
                 row(Connection.TRANSACTION_REPEATABLE_READ, Connection.TRANSACTION_READ_COMMITTED),
+
 //                InnoDB 의 경우, SERIALIZABLE 에서 모든 select 가 select ... for share 로 치환됩니다.
 //                해서 위키의 시나리오가 MySQL innoDB 에서는 어플리케이션 데드락을 발생시키는 바, 결과를 눈으로 확인할 순 없습니다.
 //                row(Connection.TRANSACTION_SERIALIZABLE, Connection.TRANSACTION_READ_UNCOMMITTED)
@@ -122,27 +40,62 @@ class DirtyReadTests : StringSpec({
 
             transaction(statement = Transaction::initShinsRo)
 
+            val shinsRoInbound = Channel<TxScenario>()
+            val karinaInbound = Channel<TxScenario>()
+
             val shinsRoJob = launch {
-                receiveDirectivesUntilCommit(userForShins, shinsRoIsolation)
+                receiveTxScenarios(
+                    txIdentifier = userForShins,
+                    isolation = shinsRoIsolation,
+                    dbSession = Database.connect(url = dbUrl, user = userForShins),
+                    channel = shinsRoInbound
+                )
             }
 
             val karinaJob = launch {
-                receiveDirectivesUntilCommit(userForKarina, karinaIsolation)
+                receiveTxScenarios(
+                    txIdentifier = userForKarina,
+                    isolation = karinaIsolation,
+                    dbSession = Database.connect(url = dbUrl, user = userForKarina),
+                    channel = karinaInbound
+                )
             }
 
-            val shinsRoInbound = coroutineInBoundChannels[userForShins]!!
-            val karinaInbound = coroutineInBoundChannels[userForKarina]!!
+            // 1. 신스로가 신스로의 휴가일수를 조회
+            shinsRoInbound.send(TxScenario { username ->
+                val daysOff = Employees.findDaysOff(shinsRoFullname)
 
-            shinsRoInbound.send(directives.selectShinsRoOffDays)
+                daysOff shouldBeExactly 0
+                println("[$username] $shinsRoFullname 의 휴가일수는 $daysOff 입니다.")
+            })
 
-            delay(1000)
-            karinaInbound.send(directives.increaseShinsRoOffDaysOne)
+            // 2. 카리나가 신스로의 휴가일수를 +1
+            karinaInbound.send(TxScenario { username ->
+                val daysOff = Employees.increaseDaysOff(shinsRoFullname, 1)
 
-            delay(1000)
-            shinsRoInbound.send(directives.selectShinsRoOffDays)
+                daysOff shouldBeExactly 1
+                println("[$username] $shinsRoFullname 의 휴가일수를 +1 했습니다.")
+            })
 
-            shinsRoInbound.send(directives.commit)
-            karinaInbound.send(directives.commit)
+            // 3. 신스로가 신스로의 휴가일수를 조회
+            shinsRoInbound.send(TxScenario { username ->
+                val daysOff = Employees.findDaysOff(shinsRoFullname)
+
+                daysOff shouldBeExactly when (shinsRoIsolation) {
+                    Connection.TRANSACTION_READ_UNCOMMITTED -> 1
+                    Connection.TRANSACTION_READ_COMMITTED -> 0
+                    Connection.TRANSACTION_REPEATABLE_READ -> 0
+
+                    else -> throw NotImplementedError()
+                }
+                println("[$username] $shinsRoFullname 의 휴가일수는 $daysOff 입니다.")
+            })
+
+            shinsRoInbound.send(TxScenario(isEnd = true) { commit() })
+            karinaInbound.send(TxScenario(isEnd = true) { commit() })
+
+            shinsRoInbound.close()
+            karinaInbound.close()
 
             shinsRoJob.join()
             karinaJob.join()
