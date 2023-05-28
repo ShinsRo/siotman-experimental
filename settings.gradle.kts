18b2cad4e03224182a2b8b386cd2458d15824318
ian <siotman0728@gmail.com>
2023-05-29 05:13:18 +0900

    chore: IDE 모듈 리졸빙을 위한 recurive 모듈 인클루드

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,25 @@
 rootProject.name = "siotman-experimental"
 
-include(
-    "00_스터디",
-    "00_스터디:도메인주도개발시작하기"
-)
+recursiveInclude("00_스터디")
 
-include(
-    "80_유틸리티"
-)
+// moduleDir 를 기준으로, *.gradle* 파일이 존재한다면, 대상 모듈 및 하위 모듈을 포함한다.
+fun recursiveInclude(vararg moduleDirNames: String) = moduleDirNames.forEach { dirName ->
+    val moduleDir = file(dirName)
+    println(findSubProjectPaths(moduleDir))
+
+    include(*(findSubProjectPaths(moduleDir).toTypedArray()))
+}
+
+// *.gradle* 파일명을 기준으로 프로젝트 패스를 검색한다.
+fun findSubProjectPaths(projectDir: File, pathPrefix: String = ""): List<String> {
+    if (projectDir.isDirectory.not()) return emptyList()
+    val files = projectDir.listFiles() ?: return emptyList()
+
+    val noScripts = files.map(File::getName)
+        .find { ".gradle" in it } == null
+    if (noScripts) return emptyList()
+
+    val projectPath = "$pathPrefix${projectDir.name}"
+    return listOf(projectPath) + files.map { findSubProjectPaths(it, "$projectPath:") }.flatten()
+}
+
