ef316f19316ea8fb60a9275755266f78fa3cb402
ian <siotman0728@gmail.com>
2023-06-03 21:29:14 +0900

    fix (DDD): StringValue equals & hashCode 누락 수정

--- 00_스터디/도메인주도개발시작하기/domains/src/main/kotlin/com/shinsro/common/StringValue.kt
+++ 00_스터디/도메인주도개발시작하기/domains/src/main/kotlin/com/shinsro/common/StringValue.kt
@@ -4,4 +4,12 @@ import java.io.Serializable
 
 abstract class StringValue(private val value: String) : CharSequence by value, Serializable {
     override fun toString() = value
+    override fun hashCode() = value.hashCode()
+    override fun equals(other: Any?): Boolean {
+        if (value == other) return true
+        if (this === other) return true
+
+        if (other !is StringValue) return false
+        return value == other.value
+    }
 }
